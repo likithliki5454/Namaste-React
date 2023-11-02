f8f9d7a65b7804b1d9caf8c27e95dc8c1d51f79a
web-dev <deepak.mishra@nuware.com>
2023-11-02 20:01:40 +0530

    root-html

--- App.js
+++ App.js
@@ -5,11 +5,12 @@
 // </div>
 
 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const header = React.createElement('div', { id: "parent" },
-  [React.createElement('div', { id: "child1" }, 
-    React.createElement('h1', {}, 'am h1'))],
-    [React.createElement('div', { id: "child2" }, 
-    React.createElement('h2', {}, 'am h2'))],
+    React.createElement('div', { id: "child1" }, 
+    React.createElement('h1', {}, 'am h1'))
     )
 
 const root=ReactDOM.createRoot(document.getElementById('root'))
